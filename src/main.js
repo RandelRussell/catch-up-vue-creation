94cc313e2bd15f04590772ee75575507c91fe465
RandelRussell <u201910669@upc.edu.pe>
2024-04-12 20:23:05 -0500

    feat: added ui component libraries

--- src/main.js
+++ src/main.js
@@ -1,6 +1,36 @@
 import './assets/main.css'
 
+import PrimeVue from "primevue/config";
+
+// Add PrimeFlex
+import 'primeflex/primeflex.css';
+
+// Add PrimeVue Components
+import Card         from 'primevue/card';
+import Button       from 'primevue/button';
+import SelectButton from 'primevue/selectbutton';
+import Sidebar      from 'primevue/sidebar';
+import Avatar       from 'primevue/avatar';
+import Menu         from 'primevue/menu';
+import MenuBar      from 'primevue/menubar';
+import Toolbar      from 'primevue/toolbar';
+
+// App Theme
+import 'primevue/resources/themes/md-light-indigo/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
 import { createApp } from 'vue'
 import App from './app.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(PrimeVue, { ripple: true })
+    .component('pv-card', Card)
+    .component('pv-button', Button)
+    .component('pv-select-button', SelectButton)
+    .component('pv-sidebar', Sidebar)
+    .component('pv-avatar', Avatar)
+    .component('pv-menu', Menu)
+    .component('pv-menubar', MenuBar)
+    .component('pv-toolbar', Toolbar)
+    .mount('#app')
